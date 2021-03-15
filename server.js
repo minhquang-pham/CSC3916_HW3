eed5de0093c5c6a5196d46ea98e9d117005547a1
minhquang-pham <minhquang.pham@ucdenver.edu>
2021-03-14 23:41:34 -0600

    Update server.js

--- server.js
+++ server.js
@@ -85,6 +85,89 @@ router.post('/signin', function (req, res) {
     })
 });
 
+router.route('/movies')
+    .post(authJwtController.isAuthenticated, function (req, res) {
+        console.log(req.body);
+        if (!req.body.title || !req.body.year_released || !req.body.genre || !req.body.actors || req.body.actors < 3) {
+            res.json({success: false, message: "An entry requires a title, the year released, the genre, and 3 actors"});
+        } else {
+            var movie = new Movie();
+
+            movie.title = req.body.title;
+            movie.year_released = req.body.year_released;
+            movie.genre = req.body.genre;
+            movie.actors = req.body.actors;
+            res.json({success: true, msg: 'Movie saved.'});
+
+        }
+    })
+    .put(authJwtController.isAuthenticated, function(req, res) {
+        if(!req.body.title){
+            res.json({success:false, message: "Please provide a title to update"});
+        }else{
+            if (req.body.newTitle){    
+                Movie.findOneAndUpdate(req.body.title, req.body.newTitle, function(err, movie) {
+                    if(err){
+                        res.status(403).json({success:false, message: "Can not update Movie"});
+                    }else{
+                        res.status(200).json({success: true, message: "Updated movie title"});
+                    }
+                });
+            }
+            if (req.body.newYear){    
+                Movie.findOneAndUpdate(req.body.year, req.body.newYear, function(err, movie) {
+                    if(err){
+                        res.status(403).json({success:false, message: "Can not update Movie"});
+                    }else{
+                        res.status(200).json({success: true, message: "Updated movie year"});
+                    }
+                });
+            }
+            if (req.body.newGenre){    
+                Movie.findOneAndUpdate(req.body.genre, req.body.newGenre, function(err, movie) {
+                    if(err){
+                        res.status(403).json({success:false, message: "Can not update Movie"});
+                    }else{
+                        res.status(200).json({success: true, message: "Updated movie genre"});
+                    }
+                });
+            }
+        }
+    })
+    .delete(authJwtController.isAuthenticated, function(req, res) {
+        if(!req.body.title){
+            res.json({success:false, message: "Please provide a title to delete"});
+        }else{
+            Movie.findOneAndDelete(req.body.title, function(err, movie) {
+                if(err){
+                    res.status(403).json({success:false, message: "Error: Could not delete"});
+                }else if(!movie){
+                    res.status(403).json({success: false, message: "Error: Movie not found"});
+                }else {
+                    res.status(200).json({success: true, message: "Movie deleted"});
+                }
+            })
+            }
+
+    })
+    .get(authJwtController.isAuthenticated, function (req, res) {
+        if (!req.body){
+            res.json({success:false, message: "Please provide a title to update"});
+        }else{
+            Movie.find(req.body).select("title year genre actors").exec(function(err, movie) {
+                if (err) {
+                    res.status(403).json({success: false, message: "Error: Could not find movie"});
+                }
+                if (movie) {
+                    res.status(200).json({success: true, message: "Movie found", Movie: movie})
+                } else {
+                    res.status(404).json({success: false, message: "Movie not found"});
+
+                }
+            })
+        }
+    })
+
 app.use('/', router);
 app.listen(process.env.PORT || 8080);
 module.exports = app; // for testing only
